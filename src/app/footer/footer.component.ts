b509ae5e2680aa14e8a11a747796047b5d72710a
Harry Seong <harry@northwestern.edu>
2019-01-04 14:04:18 -0600

    Added transition animations to navbar and footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  animations: [
+    trigger('transitionAnimations', [
+      transition(':enter', [
+        style({transform: 'translateY(4.5em)'}),
+        animate('750ms ease'),
+      ]),
+    ]),
+  ]
 })
 export class FooterComponent implements OnInit {
 
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../shared/services/firebase/auth/auth.service';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  animations: [
+    trigger('transitionAnimations', [
+      transition(':enter', [
+        style({transform: 'translateY(-3em)'}),
+        animate('750ms ease'),
+      ]),
+    ])
+  ]
 })
 export class NavbarComponent implements OnInit {
 

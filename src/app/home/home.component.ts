340081f0a36db73fc23407d4aad73feaa86482bf
Harry Seong <harry@northwestern.edu>
2019-01-04 14:18:03 -0600

    Updates to animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,11 +7,9 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
   styleUrls: ['./home.component.css'],
   animations: [
     trigger('transitionAnimations', [
-      state('fadeInDrop', style({ opacity: 1, transform: 'translateY(0.5em)',
-      })),
       transition(':enter', [
-        style({ opacity: 0 }),
-        animate('2000ms ease', style({ opacity: 1, transform: 'translateY(0.5em)'}))
+        style({ opacity: 0, transform: 'translateY(-0.5em)' }),
+        animate('2000ms ease', style({ opacity: 1, transform: 'translateY(0)'}))
       ]),
     ]),
     trigger('videoLoadAnimation', [
@@ -26,16 +24,12 @@ export class HomeComponent implements OnInit {
   @ViewChild('video') videoElement: any;
   video: any;
   vidStatus: string;
-  videoBgAnim = '';
-  transition = '';
 
   constructor() { }
 
   ngOnInit() {
     this.video = this.videoElement.nativeElement;
     this.setVidStatus();
-    this.transition = 'fadeInDrop';
-    this.videoBgAnim = 'fadeIn';
   }
 
   setVidStatus() {

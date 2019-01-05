6a24441e73765f96e4e0e4a9a479bff788ba0c07
Harry Seong <harry@northwestern.edu>
2019-01-05 14:40:10 -0600

    Places page update

--- src/app/pages/places/places.component.ts
+++ src/app/pages/places/places.component.ts
@@ -18,13 +18,13 @@ import {environment} from '../../../environments/environment';
     trigger('contentAnimations', [
       transition(':enter', [
         style({ opacity: 0 }),
-        animate('1.5s .5s ease', style({ opacity: 1 })),
+        animate('1.5s 1s ease', style({ opacity: 1 })),
       ]),
     ]),
     trigger('btnAnimations', [
       transition(':enter', [
         style({ opacity: 0, transform: 'translateY(2em)'}),
-        animate('2s ease', style({ opacity: 1, transform: 'translateY(0)' })),
+        animate('1.5s 1s ease', style({ opacity: 1, transform: 'translateY(0)' })),
       ]),
     ])
   ]

a5eb5a2bd593341f12bf2d64ffc3f54894137fe2
Harry Seong <harry@northwestern.edu>
2019-01-06 16:36:12 -0600

    About page animation updates

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -17,7 +17,31 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
         style({ opacity: 0, transform: 'translateY(0.5em)'}),
         animate('1.5s .5s ease', style({ opacity: 1, transform: 'translateY(0)' })),
       ]),
-    ])
+    ]),
+    trigger('educationAnimations', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(0.5em)'}),
+        animate('1.5s .75s ease', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+    ]),
+    trigger('experienceAnimations', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(0.5em)'}),
+        animate('1.5s 1s ease', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+    ]),
+    trigger('techAnimations', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(0.5em)'}),
+        animate('1.5s 1.25s ease', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+    ]),
+    trigger('websiteAnimations', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(0.5em)'}),
+        animate('1.5s 1.5s ease', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+    ]),
   ]
 })
 export class AboutComponent implements OnInit {

87ea028777d1ea754bc51dddccedf7457727ccf7
Harry Seong <harry@northwestern.edu>
2019-01-19 21:23:00 -0600

    Weather page updates

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -15,7 +15,7 @@ import {MatSnackBar} from '@angular/material';
     trigger('contentAnimations', [
       transition(':enter', [
         style({ opacity: 0, transform: 'translateY(0)'}),
-        animate('1s ease', style({ opacity: 1, transform: 'translateY(0)' })),
+        animate('1s 1s ease', style({ opacity: 1, transform: 'translateY(0)' })),
       ]),
     ])
   ]
